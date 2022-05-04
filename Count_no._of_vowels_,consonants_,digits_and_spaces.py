0d37a89b7cbaa6af3c204c90e1da506826ccc4f4
21A91A1221 <97592122+21A91A1221@users.noreply.github.com>
2022-05-04 19:56:05 +0530

    s=input() v=c=d=w=0 for i in s:     if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):         v=v+1     elif((i>='a' and i<='z')or(i>='A' and i<='Z')):         c=c+1     elif(i.isdigit()):         d=d+1     elif(i==' '):         w=w+1 print("Vowels:",v) print("Consonants:",c) print("Digits:",d) print("White spaces:",w)

---
+++ Count_no._of_vowels_,consonants_,digits_and_spaces.py
@@ -0,0 +1,16 @@
+s=input()
+v=c=d=w=0
+for i in s:
+    if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
+        v=v+1
+    elif((i>='a' and i<='z')or(i>='A' and i<='Z')):
+        c=c+1
+    elif(i.isdigit()):
+        d=d+1
+    elif(i==' '):
+        w=w+1
+print("Vowels:",v)
+print("Consonants:",c)
+print("Digits:",d)
+print("White spaces:",w)
+    
