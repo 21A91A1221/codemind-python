3db41cb2be85656ff4248b657acf4f592c6107f8
21A91A1221 <97592122+21A91A1221@users.noreply.github.com>
2022-06-30 10:49:57 +0530

    def amc(n):     s=0     for i in range(1,n):         if n%i==0:             s+=i     return s n=int(input()) b=int(input()) if amc(n)==b and amc(b)==n:     print("Amicable") else:     print("Not Amicable")

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,15 +1,12 @@
-n1=int(input())
-n2=int(input())
-a=0
-for i in range(1,(n1//2)+1,1):
-    if(n1%i==0):
-        a=a+i
-b=0
-for i in range(1,(n2//2)+1,1):
-    if(n2%i==0):
-        b=b+i
-if(a==n2 and b==n1):
+def amc(n):
+    s=0
+    for i in range(1,n):
+        if n%i==0:
+            s+=i
+    return s
+n=int(input())
+b=int(input())
+if amc(n)==b and amc(b)==n:
     print("Amicable")
 else:
     print("Not Amicable")
-    
