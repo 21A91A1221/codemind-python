01747f92c664188e7cd66378b0af5a57bd418ccd
21A91A1221 <97592122+21A91A1221@users.noreply.github.com>
2022-06-30 10:31:11 +0530

    n=int(input()) sq=n**2 s=0 temp=sq while temp>0:     rem=temp%10     s+=rem     temp//=10 if s==n:     print("Neon Number") else:     print("Not Neon Number")

--- Neon_Number.py
+++ Neon_Number.py
@@ -1,11 +1,12 @@
 n=int(input())
-sum=0
-sqr=n*n
-while(sqr!=0):
-    r=sqr%10
-    sum=sum+r
-    sqr=sqr//10
-if(sum==n):
-        print("Neon Number")
+sq=n**2
+s=0
+temp=sq
+while temp>0:
+    rem=temp%10
+    s+=rem
+    temp//=10
+if s==n:
+    print("Neon Number")
 else:
     print("Not Neon Number")
