7ebca0602d7faefea399bbfb97bf1cde02676051
21A91A1221 <97592122+21A91A1221@users.noreply.github.com>
2022-06-30 11:19:22 +0530

    a=int(input()) n=0 s=1 c=0 for i in range(a):     print(n,end=" ")     c=n+s     n=s     s=c

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,9 @@
-n=int(input())
-n1=0
-n2=1
-n3=0
-for i in range(1,n+1,1):
-    print(n1,end=' ')
-    n3=n1+n2
-    n1=n2
-    n2=n3
+a=int(input())
+n=0
+s=1
+c=0
+for i in range(a):
+    print(n,end=" ")
+    c=n+s
+    n=s
+    s=c
