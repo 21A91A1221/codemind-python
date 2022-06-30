fb4e7564569d83cacbc80a2c1e1f53f54cdc60cf
21A91A1221 <97592122+21A91A1221@users.noreply.github.com>
2022-06-30 10:37:15 +0530

    a,b=map(int,input().split()) lcm=1 for i in range(10000):     if lcm%a==0 and lcm%b==0:         break     else:         lcm+=1 print(lcm)

--- LCM.py
+++ LCM.py
@@ -1,16 +1,8 @@
-i,j=map(int,input().split())
-lcm=0
-if i>j:
-    lcm=i
-    while(lcm):
-        if(lcm%i==0 and lcm%j==0):
-           print(lcm)
-           break
-        lcm=lcm+1
-else:
-    lcm=j
-    while(lcm):
-        if(lcm%i==0 and lcm%j==0):
-            print(lcm)
-            break
-        lcm=lcm+1
+a,b=map(int,input().split())
+lcm=1
+for i in range(10000):
+    if lcm%a==0 and lcm%b==0:
+        break
+    else:
+        lcm+=1
+print(lcm)
