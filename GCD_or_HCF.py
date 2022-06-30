d59cf3a97c59fc51b2bf004ba5a5896c623fa84c
21A91A1221 <97592122+21A91A1221@users.noreply.github.com>
2022-06-30 11:17:16 +0530

    a,b=map(int,input().split()) hcf=1 for i in range(1,max(a,b)):     if a%i==0 and b%i==0:         hcf=i print(hcf)

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,7 +1,6 @@
-i,j=map(int,input().split())
-while(i!=j):
-    if(i>j):
-        i=i-j
-    else:
-        j=j-i
-print(i)
+a,b=map(int,input().split())
+hcf=1
+for i in range(1,max(a,b)):
+    if a%i==0 and b%i==0:
+        hcf=i
+print(hcf)
