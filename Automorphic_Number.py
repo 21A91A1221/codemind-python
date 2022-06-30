abe55536ba959e2aaee1512d5d38388a9cde08ff
21A91A1221 <97592122+21A91A1221@users.noreply.github.com>
2022-06-30 10:46:59 +0530

    import math def auto(n):     c=0     while n>0:         r=n%10         c+=1         n//=10     return c n=int(input()) s=auto(n) sq=n*n if sq%math.pow(10,s)==n:     print("Automorphic Number") else:     print("Not an Automorphic Number")

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,12 +1,15 @@
+import math
+def auto(n):
+    c=0
+    while n>0:
+        r=n%10
+        c+=1
+        n//=10
+    return c
 n=int(input())
-t=n
-l=0
-while(n>0):
-    l+=1
-    n=n//10
-n=t*t
-n=n%(pow(10,l))
-if(t==n):
-    print('Automorphic Number')
+s=auto(n)
+sq=n*n
+if sq%math.pow(10,s)==n:
+    print("Automorphic Number")
 else:
-    print('Not an Automorphic Number')
+    print("Not an Automorphic Number")
