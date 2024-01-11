14ca0cff3085ef9a718881432e9bf7d5257318ba
NitinReddy <97592122+21A91A1221@users.noreply.github.com>
2024-01-11 10:47:36 +0530

    def squares(num):     for i in range(int(num**0.5) + 1):         for j in range(i, int(num**0.5) + 1):             if i**2 + j**2 == num and i != j:                 return True     return False num=int(input()) r=squares(num) print(r)

--- Square_Number.py
+++ Square_Number.py
@@ -1,7 +1,9 @@
-import math
-n=int(input())
-sqr=int(math.sqrt(n))
-if(sqr*sqr==n):
-    print(True)
-else:
-    print(False)
+def squares(num):
+    for i in range(int(num**0.5) + 1):
+        for j in range(i, int(num**0.5) + 1):
+            if i**2 + j**2 == num and i != j:
+                return True
+    return False
+num=int(input())
+r=squares(num)
+print(r)
