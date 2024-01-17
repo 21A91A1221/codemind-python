e2470f59da1324cb233af10535bb5c9bf22fbd4f
NitinReddy <97592122+21A91A1221@users.noreply.github.com>
2024-01-17 10:06:22 +0530

    def is_isogram(s):     s = s.lower()     char_set = set()     for char in s:         if char in char_set:             return False         char_set.add(char)     return True s = input() r = is_isogram(s) print(r)

--- Isogram_or_not.py
+++ Isogram_or_not.py
@@ -1,11 +1,11 @@
 def is_isogram(s):
-    s=s.lower()
-    char_set=set()
+    s = s.lower()
+    char_set = set()
     for char in s:
         if char in char_set:
             return False
         char_set.add(char)
     return True
-s=input()
-r=is_isogram(s)
+s = input()
+r = is_isogram(s)
 print(r)
